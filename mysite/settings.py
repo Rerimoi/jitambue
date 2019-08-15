80477d7dab9d88ccf2dc5aadc5d3d769e28042e4
rerimoi <prkwonyike@gmail.com>
2019-08-15 22:52:16 +0300

    set respective url settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,4 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# spacifies where user is redirect once the have logged in
+LOGIN_REDIRECT_URL = '/'
 
