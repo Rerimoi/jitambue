fe87670e3a52f11a8d5060a9de4113bad8b6c38b
rerimoi <prkwonyike@gmail.com>
2019-08-23 01:10:17 +0300

    changed header and title in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Category, Article
+from .models import Category, Article,UserProfileInfo
 
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Article)
+admin.site.register(UserProfileInfo)
+admin.site.site_header="Jitambue"
+admin.site.site_title="Jitambue"
