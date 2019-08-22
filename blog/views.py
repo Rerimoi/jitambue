b2581819df0e711a562edc7bb5bd02871d3ba265
rerimoi <prkwonyike@gmail.com>
2019-08-23 01:12:09 +0300

    set viwes for added pages

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,63 @@
 from django.shortcuts import render,redirect
 from django.views import View
-from .models import Category, Article
+from .models import Category, Article, UserProfileInfo
+from . import forms
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/base.html', {})
+    return render(request, 'home.html', {})
+
+class locateClinic(View):
+    template = 'locateClinic.html'
+    
+    def get(self,request):
+        return render(request,self.template, {})
+
+
+class Signup(View):
+
+    template = "registration/signup.html"
+    form_class = forms.Signup
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template, {"form":form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        username = request.POST["username"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name =last_name
+        user.save()
+        return redirect("login")
+
+class Editprofile(View):
+    form_class=forms.UserProfile
+    template="userProfile.html"
+
+    def get(self,request):
+        form=self.form_class(None)
+        return render(request, self.template, {"form":form})
+
+    def post(self,request):
+        form=self.form_class(request.POST, request.FILES)
+        username= request.user
+        print(username, request.POST, request.FILES)
+        # user=User.objects.get(username=username)
+        # print(user)
+        # profilePic= request.FILES["Profile_picture"]
+        # sex= request.POST["sex"]
+        # dateOfBirth= request.POST["Date_Of_birth"]
+        # userProfile = UserProfileInfo.objects.create(profile_pic=profilePic, user=user, dateOfBirth=dateOfBirth,sex=sex)
+        # userProfile.save() 
+        return render(request, self.template, {"form":form})
+
 
 class Articles(View):
     template = 'articles.html'
