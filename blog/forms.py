c523577feb235c534d3069d7ad184b2440ff6f98
rerimoi <prkwonyike@gmail.com>
2019-08-28 16:58:17 +0300

    removed profile form

--- blog/forms.py
+++ blog/forms.py
@@ -1,26 +1,13 @@
 from django.contrib.auth.models import User
 from django import forms
-from . import models
+# from blog.models import Profile
 
 class Signup(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "password"]
 
-class UserProfile(forms.ModelForm):
-    # SEX_CHOICES= (
-    #     ('M', 'Male'),
-    #     ('F', 'Female')
-    # )
-    # sex = forms.CharField(widget=forms.Select(choices=SEX_CHOICES))
-    class Meta:
-        model = models.UserProfileInfo
-        fields = ["profile_pic","dateOfBirth","sex"]
-        # dateOfBirth = forms.DateField(
-        #     widget = forms.DateInput(format='%m%d%Y', attrs={'class':'datepicker'}),
-        #     input_formats=('%m%d%Y',)
-        # )
-        widgets = {
-            'dateOfBirth': forms.DateInput(format='%m%d%Y', attrs={'class':'datepicker'}),
-            'sex':forms.Select(attrs={'class':'select'})
-        }
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ('profile_pic', 'birth_date', 'sex')
