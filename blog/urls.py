07bc91531b08c1eb605f43805a6278faa3158f41
rerimoi <prkwonyike@gmail.com>
2019-08-28 16:58:37 +0300

    removed userprofile url

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,11 @@ app_name= 'blog'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/',views.Login.as_view(), name='login'),
     path('signup/', views.Signup.as_view(), name='signup'),
     path('knowzone/', views.contents.as_view(), name='knowzone'),
     path('knowzone/articles/<int:category_id>/', views.Articles.as_view(), name='articles'),
     path('knowzone/articles_details/<int:article_id>/', views.Article_detail.as_view(), name='article_detail'),
-    path('userprofile/', views.Editprofile.as_view(), name='userprofile'),
     path('locate_clinic/', views.locateClinic.as_view(), name='locate_clinic')
     
 ]
