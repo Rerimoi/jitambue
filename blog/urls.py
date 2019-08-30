30b275000cad10dd9769ee20327fe40ebfd64295
rerimoi <prkwonyike@gmail.com>
2019-08-30 10:26:53 +0300

    changed default path to sign up page

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from django.views.generic import TemplateView
 app_name= 'blog'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Signup.as_view(), name='signup'),
     path('login/',views.Login.as_view(), name='login'),
-    path('signup/', views.Signup.as_view(), name='signup'),
+    path('home/', views.home, name='home'),
     path('knowzone/', views.contents.as_view(), name='knowzone'),
     path('knowzone/articles/<int:category_id>/', views.Articles.as_view(), name='articles'),
     path('knowzone/articles_details/<int:article_id>/', views.Article_detail.as_view(), name='article_detail'),
